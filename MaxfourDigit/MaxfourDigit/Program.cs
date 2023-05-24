5a876dbd5297a18535313c23ecc3db22b2975926
siva <siva.thota@cognine.com>
2023-05-24 12:40:14 +0530

    firstcomit

---
+++ MaxfourDigit/MaxfourDigit/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace Maxfour
+{
+    class Fourdigit
+    {
+
+        string input = "3489794532324";
+        int[] array = new int[20];
+        int index = 0;
+        int max = 0;
+        public static void Main(string[] args)
+        {
+            Fourdigit obj_Digit = new Fourdigit();
+
+            if (obj_Digit.input.Length < 4)
+            {
+                Console.WriteLine("not possible");
+            }
+
+            else if(obj_Digit.input.Length==4)
+            {
+                Console.WriteLine(obj_Digit.input);
+            }
+
+            else 
+            {
+                //input values dupping into the array by converting in to int;
+                while (obj_Digit.index != obj_Digit.input.Length)
+                {
+                    int num = (int)obj_Digit.input[obj_Digit.index]-48;
+
+                    obj_Digit.array[obj_Digit.index] = num;
+                    obj_Digit.index += 1;
+
+                }
+
+                // product of four adjecent digits
+                for (int j_index = 0; j_index < obj_Digit.array.Length - 3; j_index++)
+                {
+                    int fourdigit_product = obj_Digit.array[j_index] * obj_Digit.array[j_index + 1] *obj_Digit.array[j_index + 2] * obj_Digit.array[j_index + 3];
+
+                    //it checks which is max product
+                    if (fourdigit_product > obj_Digit.max)
+                    {
+                        obj_Digit.max = fourdigit_product;
+                    }
+                }
+                Console.WriteLine(obj_Digit.max);
+            }
+        }
+        
+    }
+}
---
+++ SubStrings/SubStrings/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace Str
+{
+    public class Substrings
+    {
+        
+        string main_str ="";
+        string sub_str = "ab";
+        int count = 0;
+        public static void Main(string[] args)
+        {
+            Substrings str_obj = new Substrings();
+
+            Console.WriteLine("enter the string");
+            str_obj.main_str = Console.ReadLine();
+
+            int main_len = str_obj.main_str.Length;
+            int sub_len = str_obj.sub_str.Length;
+
+            //checking the len of main_str and sub_str
+            if (main_len<sub_len)
+            {
+                Console.WriteLine("not possible");
+            }
+
+            //to split main_str to substring and checking with the sub_string
+            for(int index=0 ; index<main_len +1 - sub_len ; index++)
+            {
+                string s3 = str_obj.main_str.Substring(index, sub_len);
+
+                if (str_obj.sub_str == s3)
+                {
+                    str_obj.count += 1;
+                    Console.WriteLine(index);
+                }
+                
+            }
+            Console.WriteLine(str_obj.count);
+        }
+    }
+}
